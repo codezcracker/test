d349904fef9608194c6f0fa82a63f8bf409c3071
codezcracker <36415183+codezcracker@users.noreply.github.com>
2024-06-06 04:37:00 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ value }),
+      body: JSON.stringify({ message }),
     });
     
     const data = await res.json();
